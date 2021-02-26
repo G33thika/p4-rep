71910de534d95fe0910aeb61cfd771b1d2137faf
Geethika Wijesuriya <geethikawijesuriya@icloud.com>
2021-02-27 04:06:49 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   output.txt 	new file:   twitter_bot.py

---
+++ twitter_bot.py
@@ -0,0 +1,23 @@
+import tweepy
+import time
+import os
+from os import environ
+
+
+consumer_key = environ['consumer_key']
+consumer_secret = environ['consumer_secret']
+access_token = environ['access_token']
+access_token_secret = environ['access_token_secret']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+#print(user)
+
+for follower in tweepy.Cursor(api.followers).items():
+    print(follower.name, file=open("output.txt", "w"))
+    

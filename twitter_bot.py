98a6560e5f86e5fdb7c7f0e2facd00ef2dc12502
Geethika Wijesuriya <geethikawijesuriya@icloud.com>
2021-02-27 20:43:15 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -2,12 +2,29 @@ import tweepy
 import time
 import os
 from os import environ
+import psycopg2
+
 
 
 consumer_key = environ['consumer_key']
 consumer_secret = environ['consumer_secret']
 access_token = environ['access_token']
 access_token_secret = environ['access_token_secret']
+
+db = environ['db']
+user = environ['username']
+dbpass = environ['dbpass']
+host = environ['host']
+port = environ['port']
+
+conn = psycopg2.connect(
+    database=db, user=user, password=dbpass, host=host, port=port
+)
+
+
+conn.autocommit = True
+
+cursor = conn.cursor()
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -16,11 +33,16 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 user = api.me()
 
-#print(user)
-# file = open("output.txt", "w")
-# file.write("")
-# file.close()
 
 for follower in tweepy.Cursor(api.followers).items():
-    print(follower.name, file=open("output.txt", "a", encoding='utf-8'))
-    
+    # print(follower.name, file=open("ou.txt", "a", encoding='utf-8'))
+    query = "INSERT INTO followers (id, name) VALUES (%s, %s);"
+    data = (follower.id , follower.name)
+
+    try:
+        cursor.execute(query, data)
+        conn.commit()
+    except Exception as error:
+        pass
+
+conn.close()
